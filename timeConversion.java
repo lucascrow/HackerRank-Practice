f999c7618a510f860210fabac599e036f44211b7
lucascrow <101023191+lucascrow@users.noreply.github.com>
2023-07-19 11:23:18 -0500

    Update timeConversion.java

--- timeConversion.java
+++ timeConversion.java
@@ -20,8 +20,54 @@ class Result {
      */
 
     public static String timeConversion(String s) {
-    // Write your code here
+    String newTime = s.toString();
+        
+        if (s.contains("AM"))
+        {
+            for (int i = 0; i < s.length()-2; i++)
+            {
+                newTime += s.charAt(i);
+            }
+            
+        } /*else for (int i = 0; i < s.length()-2; i++) {
+  
+            switch (s.charAt(i)) {
+            case 1:  i = '';
+                     break;
+            case 2:  monthString = "February";
+                     break;
+            case 3:  monthString = "March";
+                     break;
+            case 4:  monthString = "April";
+                     break;
+            case 5:  monthString = "May";
+                     break;
+            case 6:  monthString = "June";
+                     break;
+            case 7:  monthString = "July";
+                     break;
+            case 8:  monthString = "August";
+                     break;
+            case 9:  monthString = "September";
+                     break;
+            case 10: monthString = "October";
+                     break;
+            case 11: monthString = "November";
+                     break;
+            case 12: monthString = "December";
+                     break;
+            default: monthString = "Invalid month";
+                     break;
+                // Insert the string to be inserted
+                // into the new string
+                newString += stringToBeInserted;
+            }
+            
+            newTime += s.charAt(i);*/
 
+        }
+    System.out.println(newTime);
+    return newTime;
     }
 
 }
